783190e97a3657ad2a17174e7d0f01f586412eb1
Asus <Asus@LAPTOP-F814NMF7>
2024-07-02 15:46:25 +0400

    Add project files.

---
+++ homework 1st/homework 1st.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\tResume:\n" << endl;
+    cout << "name:Eli surname:Babayev age:15 hobby:gaming" << endl;
+
+
+    cout << "\t  - - -\n\t/\t\\ \n\       /         \\";
+    cout << "\n       |         |";
+    cout << "\n       |         |";
+    cout << "\n       |         |";
+    cout << "\n       \\ \t /";
+    cout << "\n\t\\ \t/\n\t  - - -\n";
+
+
+    cout << "\"To be \n\tor not\n\t\tto be!\"\n\t\t\t/Shekespeare/\n";
+
+    cout << "\tEscape seguences\n";
+    cout << "\\a\tBell (alert)\n\\b\tBackspace\n\\n\tNew line\n\\r\tCarriace return\n\\t\tHorizantal tab\n\\\\\tBackslash\\\n\\\"\tDubble quartation mark \\\"\n\\\'\tSingle quartation mark \\\'\n";
+    cout << "************   *              *\n           *   *\n           *   *              *\n           *   *              *\n************   *              *\n*          *   *              *\n*          *   *              *\n************   ***********    *\n";
+    cout << "\" \'\\t\',  \'\\n\\',  \'\\\\\',  '\\'',  '\\\"' \"";
+
+    cout << "\n	********************\n       *|                 **\n      * |                * *\n     *  |               *  *\n    *   |              *   *\n   *    |             *    *\n  *     |            *     *\n ********************      *\n *      |           *      *\n *      |           *      *\n *      |           *      *\n *      |           *      *\n *      |           *      *\n *      |           *      *\n *      |           *      *\n *      |           *      *\n *      |           *      *\n *      |           *      *\n *      |           *      *\n *      |___________*______*\n *     /            *     *\n *    /             *    *\n *   /              *   *\n *  /               *  *\n * /                * *\n */                 **\n ********************\n";
+
+    cout << "\#include <iosteam>\nusing namespace std;\nvoid main<>\n{\n\tcout << \"C++ forever!!!\";\n}";
+
+
+    return 0;
+}
